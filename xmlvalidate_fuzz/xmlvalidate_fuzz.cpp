07fd27242258d5ac50b25466dab8b9ab38e47d21
Keylor <keylorx.morera.rodrigurez@intel.com>
2019-11-07 14:34:01 -0600

    test

---
+++ xmlvalidate_fuzz/xmlvalidate_fuzz.cpp
@@ -0,0 +1,101 @@
+// xmlvalidate_fuzz.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#import <msxml6.dll>
+extern "C" __declspec(dllexport)  int main(int argc, char** argv);
+
+// Macro that calls a COM method returning HRESULT value.
+#define CHK_HR(stmt)        do { hr=(stmt); if (FAILED(hr)) goto CleanUp; } while(0)
+
+void dump_com_error(_com_error &e)
+{
+	_bstr_t bstrSource(e.Source());
+	_bstr_t bstrDescription(e.Description());
+
+	printf("Error\n");
+	printf("\a\tCode = %08lx\n", e.Error());
+	printf("\a\tCode meaning = %s", e.ErrorMessage());
+	printf("\a\tSource = %s\n", (LPCSTR)bstrSource);
+	printf("\a\tDescription = %s\n", (LPCSTR)bstrDescription);
+}
+
+_bstr_t validateFile(_bstr_t bstrFile)
+{
+	// Initialize objects and variables.
+	MSXML2::IXMLDOMDocument2Ptr pXMLDoc;
+	MSXML2::IXMLDOMParseErrorPtr pError;
+	_bstr_t bstrResult = L"";
+	HRESULT hr = S_OK;
+
+	// Create a DOMDocument and set its properties.
+	CHK_HR(pXMLDoc.CreateInstance(__uuidof(MSXML2::DOMDocument60), NULL, CLSCTX_INPROC_SERVER));
+
+	pXMLDoc->async = VARIANT_FALSE;
+	pXMLDoc->validateOnParse = VARIANT_TRUE;
+	pXMLDoc->resolveExternals = VARIANT_TRUE;
+
+	// Load and validate the specified file into the DOM.
+	// And return validation results in message to the user.
+	if (pXMLDoc->load(bstrFile) != VARIANT_TRUE)
+	{
+		pError = pXMLDoc->parseError;
+
+		bstrResult = _bstr_t(L"Validation failed on ") + bstrFile +
+			_bstr_t(L"\n=====================") +
+			_bstr_t(L"\nReason: ") + _bstr_t(pError->Getreason()) +
+			_bstr_t(L"\nSource: ") + _bstr_t(pError->GetsrcText()) +
+			_bstr_t(L"\nLine: ") + _bstr_t(pError->Getline()) +
+			_bstr_t(L"\n");
+	}
+	else
+	{
+		bstrResult = _bstr_t(L"Validation succeeded for ") + bstrFile +
+			_bstr_t(L"\n======================\n") +
+			_bstr_t(pXMLDoc->xml) + _bstr_t(L"\n");
+	}
+
+CleanUp:
+	return bstrResult;
+}
+
+wchar_t* charToWChar(const char* text)
+{
+	size_t size = strlen(text) + 1;
+	wchar_t* wa = new wchar_t[size];
+	//mbstowcs_s(wa, text, size);
+	mbstowcs(wa, text, size);
+	return wa;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		printf("Usage: %s <xml file>\n", argv[0]);
+		return 0;
+	}
+
+	HRESULT hr = CoInitialize(NULL);
+	if (SUCCEEDED(hr))
+	{
+		try
+		{
+			_bstr_t bstrOutput = validateFile(charToWChar(argv[1]));
+			MessageBoxW(NULL, bstrOutput, L"noNamespace", MB_OK);
+		}
+		catch (_com_error &e)
+		{
+			dump_com_error(e);
+		}
+		CoUninitialize();
+	}
+
+	return 0;
+
+}
+
+
+
